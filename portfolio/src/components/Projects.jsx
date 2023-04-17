94ee7ee025a3acc6c1a94a14446ec48342a8415a
Dmitriy Angve <kirirch129@gmail.com>
2023-04-17 10:11:00 +0300

    images

---
+++ portfolio/src/components/Projects.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProjectItem from "./ProjectItem";
+import mealsImg from "../assets/ReactMeals.jpg";
+import forkifyImg from "../assets/Forkify.jpg";
+import bookmartImg from "../assets/Bookmart.jpg";
+import omnifoodImg from "../assets/Omnifood.jpg";
+
+const Projects = () => {
+  return <div>Projects</div>;
+};
+
+export default Projects;
